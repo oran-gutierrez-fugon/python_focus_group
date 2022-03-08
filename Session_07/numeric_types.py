1ccc46c25fe92e1d36589fb8c9030e618ad590a4
oran-gutierrez-fugon <ojgutierrez@ucdavis.edu>
2022-03-08 15:58:39 -0800

    session 7

---
+++ Session_07/numeric_types.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Integers
+int1 = 1
+print(f'The tpe for int1 is (type(int1))')
+int2 = 9
+int3 = 42
+int4 = 558
+int5 = 21543215
+
+not_int = 21,543,215
+
+print(int5, type(int5))
+print(not_int, type(not_int))
+
+# Floats
+float1 = 1.0
+float2 = 3.4
+float3 = 74.3
+float4 = 9.99999999999
+float5 = 1844384.85262
+float6 = 3/4
+float7 = 9/16
